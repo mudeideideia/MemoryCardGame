67603754b9db7bc448d4e4c6a8a606838ddbb5cc
mudeideideia <44714407+mudeideideia@users.noreply.github.com>
2020-02-06 12:12:18 -0300

    componentes retornando função

--- src/components/memory-cards/index.js
+++ src/components/memory-cards/index.js
@@ -1,8 +1,54 @@
-const createMemoryCard = ({ src, alt, nameClass}) => 
+function memoryCard () {
+
+    const style = `
+        .memory-card {
+            width: 155px;
+            height: 155px;
+            background-color: #f24a70;
+            border-radius: 30px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
+            position: relative;
+            cursor: pointer;
+        }
+
+        .memory-card.-front {
+            background-color: #fff;
+        }
+
+        .memory-card.-front::before {
+            content: "";
+            width: 95px;
+            height: 95px;
+            background: #d4d4d4;
+            border-radius: 50%;
+            position: absolute;
+        }
+
+        .memory-card > .icon {
+            width: 80px;
+            height: 80px;
+        }
+
+
+        .memory-card.-front > .icon {
+            position: absolute;
+            transform: traslateY(-12%);
+        }
     `
-    <article class="memory-card ${nameClass}">
-        <img src="${src}" alt="${alt}" class="icon" onClick="handleClick(this)"/>
-    </article>
-    `
+
+    const $style = document.createElement("style")
+    $style.textContent = style
+    document.querySelector("head").insertBefore($style, null)
+    
+    return ({ src, alt, nameClass}) => 
+        `
+            <article class="memory-card ${nameClass}">
+                <img src="${src}" alt="${alt}" class="icon" onClick="handleClick(this)"/>
+            </article>
+        `
+}
 
 const handleClick = (pointer) => console.log(pointer)
--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -3,19 +3,28 @@ const $root = document.querySelector("#root")
 
 const $cardWrapping = createCardWrapping();
 
-const $memoryCardGirl = createMemoryCard({
-    src: "img/girl.jpg",
+const createMemoryCard = memoryCard()
+
+const $memoryCardGeio = createMemoryCard({
+    src: "img/geio.svg",
     alt: "program girl",
     nameClass: ""
+});
+const $memoryCardGirl = createMemoryCard({
+    src: "img/girl_transparent.png",
+    alt: "program girl",
+    nameClass: "-front"
 });
 const $memoryCardBook = createMemoryCard({
     src: "img/book.svg",
     alt: "program book",
-    nameClass: ""
+    nameClass: "-front"
 });
 
 
 $cardWrapping.insertAdjacentHTML("beforeend", $memoryCardGirl)
 $cardWrapping.insertAdjacentHTML("beforeend", $memoryCardBook)
+$cardWrapping.insertAdjacentHTML("beforeend", $memoryCardGeio)
+$cardWrapping.insertAdjacentHTML("beforeend", $memoryCardGeio)
 
 $root.insertAdjacentElement("beforeend", $cardWrapping)
